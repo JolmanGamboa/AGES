ee6134f0d71f1463aabbf94a7fef314bd37ad3b5
JolmanGamboa <141514825+JolmanGamboa@users.noreply.github.com>
2025-02-14 14:18:48 -0500

    Inicio proyecto

---
+++ src/main/java/tesis/ages/AGES.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tesis.ages;
+
+/**
+ *
+ * @author jolma
+ */
+public class AGES {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
